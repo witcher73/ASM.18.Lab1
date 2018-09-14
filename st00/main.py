f1785d478c51217f24ed36204f486783f0cbd7af
Dmitry Leonov <dl@bugtraq.ru>
2018-09-14 11:58:57 +0300

    sample fix

--- st00/main.py
+++ st00/main.py
@@ -1,4 +1,12 @@
-from .group import group
+if __name__ == '__main__':
+    from group import group
+else:
+    from .group import group
+
 
 def main():
 	group().f()
+
+
+if __name__ == '__main__':
+	main()
